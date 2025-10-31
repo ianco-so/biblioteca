53f8ad62e8361881b985eb127cb5a8275e5d8d69
ianucasm <ianlucas2014t@gmail.com>
2025-10-31 12:47:43 -0300

    classe Author linkada a Book

--- src/main/model/Book.java
+++ src/main/model/Book.java
@@ -2,17 +2,17 @@ package main.model;
 
 public class Book {
     private String title;
-    private String author; 
+    private Author author;
     private String isbn;
     // private Publisher publisher; //TODO: Implementar Publisher
-    // private Author author; //TODO: Implementar Author?
 
-    public Book(String title, String author, String isbn) {
+
+    public Book(String title, Author author, String isbn) {
         if (title == null || author == null || isbn == null) { // Validação simples // TODO: retirar e usar Lombok
             throw new IllegalArgumentException("Titulo, autor e ISBN não podem ser nulos.");
         }
         this.title = title.trim();
-        this.author = author.trim();
+        this.author = author;
         this.isbn = isbn.trim(); //TODO: Validar ISBN
     }
 
@@ -20,11 +20,16 @@ public class Book {
         return this.title;
     }
 
-    public String getAuthor() {
+    public Author getAuthor() {
         return this.author;
     }
 
     public String getIsbn() {
         return this.isbn;
+    }
+
+    @Override
+    public String toString() {
+        return title + " | " + author + " | " + isbn;
     }
 }
